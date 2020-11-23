f9fc8217b32dba3aae371ce4df757c645fea3a1b
Suraj Khosla <surajkhosla79@gmail.com>
2020-11-22 19:42:03 -0600

    Update connect_4.asm

--- connect_4.asm
+++ connect_4.asm
@@ -1,346 +1,322 @@
 .data
-
-boardArray: .byte 0:42 #This will be the array that represents the gameboard: 0=Empty 1=Player1 2=Player2
-prompt0: .asciiz "Welcome to Connect 4!\nThis is a MIPS version of the classic 2 player board game Connect 4.\nThe game will begin with Player 1's turn.\nEnter 1-7 to choose which column to place the game piece in.\nOnce Player 1 goes, Player 2 may then take their turn.\nThe game will automatically alternate user turns so be patient and once you see the game peice placed on the Bitmap it is time for the other player to take their turn!\n\nHave fun!\n\n"
-prompt1: .asciiz "\nPlayer 1's turn: "
-prompt2: .asciiz "\nPlayer 2's turn: "
-prompt3: .asciiz "Player 1 Wins!\n"
-prompt4: .asciiz "Player 2 Wins!\n"
-prompt5: .asciiz "Please enter a number between 1 and 3 (inclusive)\n"
-prompt6: .asciiz "The column you have chosen is full. Select a different column\n"
-prompt7: .asciiz "It's a Tie!\n"
-
-.text
-
-#Load Welcome Prompt
-la $a0, prompt0
-li $v0, 4
-syscall
-
-################################  Begin Main ################################
-main:
-
-#Get Player 1 Input
-playerOne:
-la $a0, prompt1
-li $v0, 4
-syscall
-li $v0, 5
-syscall
-
-#Place User Input into Array and Error Check
-li $a0, 1
-jal StoreInput
-
-#Check for Player 1 "Connect 4"
-#If found, go to Player 1 win
-#If not found, continue game
-jal WinCheck
-
-#Get Player 2 Input
-playerTwo:
-la $a0, prompt2
-li $v0, 4
-syscall
-li $v0, 5
-syscall
-
-#Place User Input into Array
-li $a0, 2
-jal StoreInput
-
-#Check for Player 2 "Connect 4"
-#If found, go to Player 2 win
-#If not found, go back to Player 1 turn
-jal WinCheck
-
-j main #Play next set of turns
-################################  End Main ################################
-
-#Procedure: StoreInput
-#Input: User entered value - $v0
-#Input: Player Number (1 or 2) - $a0
-#Output: Box Number ($v0)
-#Will determine which exact array location to place user input and store it into array
-StoreInput:
-addiu $v0, $v0, -8 #Convert user input into Array notation(-1) and subtract for nextCheck Loop(-7)
-bltu $v0, -7, OOBError
-bgtu $v0, -1, OOBError
-
-#Find out (in the column) where the next available row is
-nextCheck:
-addiu $v0, $v0, 7 #Increment row
-bgtu $v0, 41, ColumnFull#If column is full go to error
-lb $t1, boardArray($v0) #Load byte from boardArray that user has chosen
-bnez $t1, nextCheck #If loaded byte is NOT EMPTY(1 or 2) then try next row up (add 7 to array index)
-
-#Only reach here if boardArray(base + offset) = 0
-sb $a0, boardArray($v0) #Place player number into boardArray at location player's chip will end
-
-jr $ra #Finished Procedure Successfully
-
-#Out of Bounds Error Catching
-OOBError:
-move $t0, $a0
-la $a0, prompt5
-li $v0, 4
-syscall
-move $a0, $t0
-j returnToPlayer
-
-#Column Full Error Catching
-ColumnFull:
-move $t0, $a0
-la $a0, prompt6
-li $v0, 4
-syscall
-move $a0, $t0
-
-returnToPlayer:
-beq $a0, 1, playerOne
-beq $a0, 2, playerTwo
-
-################################  Begin WinCheck ################################
-#Procedure: WinCheck
-#Input: $a0 - Player Number
-#Input: $v0 - Last location offset chip was placed
-#Will deterine is the current player's move have triggered a win using DFS
-WinCheck:    
-      #Must check FOUR different directions a win can happen:
-      #1. Horizontal Line
-      #2. Vertical Line
-      #3. Forward Slash
-      #4. Backward Slash
-   
-      #5. Check for Full Board (Tie)
-      addiu $sp, $sp, -4
-      sw $ra, ($sp)
-     
-        li $t8, 7 #Constant 7 used for modulo divison for left-most and right-most checking
-         
-    #-----------------Check horizontal-----------------#
-      #From start, go LEFT as far possible
-      li $t9, 1 #Counter - once reaches 4 then player-$a0 wins
-move $t2, $v0 #Copy the ORIGINAL offset into $t2 for manipulation when searching LEFT
-move $t4, $v0 #Copy the ORIGINAL offset into $t4 for manipulation when searching RIGHT
-        checkLeft:
-      la $t0, boardArray($t2) #Load our current chip address
-     
-        #If we are at the leftmost slot, skip to check right
-      div $t2, $t8
-      mfhi $t3 #The modulo result of offset value % 7
-      beqz $t3, checkRight #If result = 0 then go to check right
-     
-      #Else look at slot to our left
-      lb $t1, -1($t0) #Left of current location
-      bne $t1, $a0, checkRight #If value is not equal to player number, then proceed to check right
-      addiu $t9, $t9, 1 #Else value IS player number, increment counter and check next left
-      addiu $t2, $t2, -1
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-      j checkLeft
-     
-      #From start, go RIGHT as far possible
-checkRight:
-la $t0, boardArray($t4)
-
-#If we are at rightmost slot, end horizontal checking
-div $t4, $t8
-mfhi $t3
-beq $t3, 6, endHorz #If modulo result = 6 then we know we are in rightmost slot
-
-#Else look at slot to our right
-lb $t1, 1($t0) #Right of current location
-bne $t1, $a0, endHorz #If value is not player number, end checking
-addiu $t9, $t9, 1 #Else increment coutner
-addiu $t4, $t4, 1 #Move to next value to the right
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-j checkRight
-
-endHorz:
-#-----------------End Horizontal Check-----------------#
-
-     
-      #-----------------Check vertical-----------------#
-      #From start, go UP as far possible
-      li $t9, 1 #Counter - once reaches 4 then player-$a0 wins
-move $t2, $v0 #Copy the ORIGINAL offset into $t2 for manipulation when searching UP
-move $t4, $v0 #Copy the ORIGINAL offset into $t4 for manipulation when searching DOWN
-        checkUp:
-      la $t0, boardArray($t2) #Load our current chip address
-     
-        #If we are at the top row, skip to checkDown
-      bgtu $t2, 34, checkDown #If our offset is greater than 34 that means we are on the top row
-     
-      #Else look at slot above us
-      lb $t1, 7($t0) #Left of current location
-      bne $t1, $a0, checkDown #If value is not equal to player number, then proceed to check down
-      addiu $t9, $t9, 1 #Else value IS player number, increment counter and check next row up
-      addiu $t2, $t2, 7
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-      j checkUp
-     
-      #From start, go DOWN as far possible
-checkDown:
-la $t0, boardArray($t4)
-
-#If we are at bottom row, end vertical checking
-bltu $t4, 7, endVert
-
-#Else look at slot below us
-lb $t1, -7($t0) #Below current location
-bne $t1, $a0, endVert #If value is not player number, end checking
-addiu $t9, $t9, 1 #Else increment coutner
-addiu $t4, $t4, -7 #Move to next value below current location
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-j checkDown
-
-endVert:  
-      #-----------------End Vertical Check-----------------#
-     
-     
-     
-     
-      #-----------------Check forward-slash diagonal-----------------#
-#From start, go UP-RIGHT (UR) as far possible
-      li $t9, 1 #Counter - once reaches 4 then player-$a0 wins
-move $t2, $v0 #Copy the ORIGINAL offset into $t2 for manipulation when searching UR
-move $t4, $v0 #Copy the ORIGINAL offset into $t4 for manipulation when searching DL
-        checkUR:
-      la $t0, boardArray($t2) #Load our current chip address
-     
-        #If we are at the top row OR we are at the rightmost coloumn, then skip to down-left
-      bgtu $t2, 34, checkDL #If our offset is greater than 34 that means we are on the top row
-div $t2, $t8
-mfhi $t3
-beq $t3, 6, checkDL #If modulo result = 6 then we know we are in rightmost slot
-     
-      #Else look at slot above us and over to the right
-      lb $t1, 8($t0) #UR of current location
-      bne $t1, $a0, checkDL #If value is not equal to player number, then proceed to check right
-      addiu $t9, $t9, 1 #Else value IS player number, increment counter and check next value in pattern
-      addiu $t2, $t2, 8
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-      j checkUR
-     
-      #From start, go DOWN-LEFT (DL) as far possible
-checkDL:
-la $t0, boardArray($t4)
-
-#If we are at bottom row OR leftmost column, then end FSDiag checking
-bltu $t4, 7, endFSDiag #Bottom row test
-div $t4, $t8
-mfhi $t3
-beq $t3, 0, endFSDiag #Leftmost column test
-
-#Else look at slot below us and over to the left one
-lb $t1, -8($t0) #DL of current location
-bne $t1, $a0, endFSDiag #If value is not player number, end checking
-addiu $t9, $t9, 1 #Else increment coutner
-addiu $t4, $t4, -8 #Move to next value
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-j checkDL
-
-endFSDiag:  
-      #-----------------End Forward-Slash Diagonal Check-----------------#
-     
-     
-     
-     
-      #-----------------Check backward-slash diagonal-----------------#
-#From start, go UP-LEFT (UL) as far possible
-      li $t9, 1 #Counter - once reaches 4 then player-$a0 wins
-move $t2, $v0 #Copy the ORIGINAL offset into $t2 for manipulation when searching UL
-move $t4, $v0 #Copy the ORIGINAL offset into $t4 for manipulation when searching DR
-        checkUL:
-      la $t0, boardArray($t2) #Load our current chip address
-     
-        #If we are at the top row OR we are at the leftmost coloumn, then skip to down-right
-      bgtu $t2, 34, checkDR #Top row test
-div $t2, $t8
-mfhi $t3
-beq $t3, 0, checkDR #Left-most column test
-     
-      #Else look at slot above us and over to the left
-      lb $t1, 6($t0) #Up and Left of current position
-      bne $t1, $a0, checkDR #If value is not equal to player number, then proceed to check right
-      addiu $t9, $t9, 1 #Else value IS player number, increment counter and check next value in pattern
-      addiu $t2, $t2, 6
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-      j checkUL
-     
-      #From start, go DOWN-RIGHT (DR) as far possible
-checkDR:
-la $t0, boardArray($t4)
-
-#If we are at bottom row OR rightmost column, then end BSDiag checking
-bltu $t4, 7, endBSDiag #Bottom row test
-div $t4, $t8
-mfhi $t3
-beq $t3, 6, endBSDiag #Right-most column test
-
-#Else look at slot below us and over to the right one
-lb $t1, -6($t0) #BR of current location
-bne $t1, $a0, endBSDiag #If value is not player number, end checking
-addiu $t9, $t9, 1 #Else increment coutner
-addiu $t4, $t4, -6 #Move to next value
-bgt $t9, 3, PlayerWon #If player has more than 3 connected (so 4+), then they won
-j checkDR
-
-endBSDiag:    
-      #-----------------End Backward-Slash Diagonal Check-----------------#
-     
-      #-----------------Start Full Board Check-----------------#
-      li $t9, 35 #Load the offset for the top row of the gameboard
-      la $t0, boardArray($t9)
-     
-      li $t2, 0 #Counter for # of player chips in top row
-    checkTop:
-    lb $t1, ($t0)
-    beqz $t1, endTie #If a blank slot is found then stop checking
-    addi $t0, $t0, 1
-    add $t2, $t2, 1
-    beq $t2, 7, GameTie #If there are 7 chips in top row, it's a tie
-    j checkTop
-   
-    endTie:
-      #-----------------End Full Board Check-----------------#
-
-lw $ra, ($sp)
-addiu $sp, $sp, 4
-jr $ra #Return to game after all checks are made
-
-
-################################  End WinCheck ################################
-
-#Sub-Procedure: GameTie
-#Triggered when the top row of game board is filled and no one is a winner
-GameTie:
-la $a0, prompt7
-li $v0, 4
-syscall
-li $v0, 10
-syscall
-
-
-#Procedure: PlayerWon
-#Input: $a0 - Player Number
-#Triggered when a player wins a game
-#Will show winner message then exit program
-PlayerWon:
-beq $a0, 1 player1Win #If player 1 won, jump to second instruction set
-
-#Player 2 Won
-la $a0, prompt4
-li $v0, 4
-syscall
-li $v0, 10
-syscall
-
-#Player 1 Won
-player1Win:
-la $a0, prompt3
-li $v0, 4
-syscall
-li $v0, 10
-syscall
+	gamespace: .byte 0:18 # 6x3 game space
+	welcomeprompt: .asciiz "3 in a Line: A MIPS version. Enter a column number (1, 2, or 3) to get started. \n"
+	humanprompt: .asciiz "It's your turn: "
+	cpuprompt: .asciiz "It's the computer's turn. The computer played: "
+	humanwintext: .asciiz "You've beaten the computer. Congrats! \n"
+	cpuwintext: .asciiz "You've lost to the computer. Tough luck. \n"
+	tietext: .asciiz "The game is tied. \n"
+	invalidinput: .asciiz "Input must be an integer between 1 and 3, inclusive: \n"
+	full: .asciiz "The column you chose is full. Choose a different column: \n"
+	separate: .asciiz " | "
+	newline: .asciiz "\n"
 	
+.text
+	la $a0, welcomeprompt		#print opening statement and gamespace
+	li $v0, 4
+	syscall
+	jal Print
+	
+	turns:
+		human:
+			la $a0, humanprompt		#prompt for input
+			li $v0, 4
+			syscall
+			li $v0, 5			#read in input
+			syscall
+			la $a0, 1			#pass player num as argument then check for validity
+			jal storeturn			#store play if valid
+			jal Print			#print the gamespace
+			jal WinCheck			#check for a win
+	
+		computer:
+			la $a0, cpuprompt		#alert that it's the computer's turn
+			li $v0, 4
+			syscall
+			li $a1, 3			#randomly generate number between 1-3
+			li $v0, 42
+			syscall
+			add $a0, $a0, 1
+			li $v0, 1			#print the number
+			syscall
+			move $t0, $a0
+			la $a0, newline
+			li $v0, 4
+			syscall
+			move $v0, $t0			#pass play and player num as arguments
+			la $a0, 2
+			jal storeturn
+			jal Print
+			jal WinCheck
+	
+	j turns					#begin next round after both players play
+
+
+# storeturn subroutine, takes input in v0 and player num in a0
+storeturn:
+	addiu $v0, $v0, -4		#checks if input is within bounds, error if not
+	bltu $v0 -3, invalidinputerror
+	bgtu $v0, -1, invalidinputerror
+	
+	nextCheck:			
+	addiu $v0, $v0, 3		#check next row up
+	bgtu $v0, 17, colfull		#if column is full there is an error
+	lb $t1, gamespace($v0)
+	bnez, $t1, nextCheck		#if row is not empty, check next row
+	
+	sb $a0, gamespace($v0)		#store play if it made it through checks
+	
+	jr $ra
+	
+	invalidinputerror:
+	move $t0, $a0
+	la $a0, invalidinput
+	li $v0, 4
+	syscall
+	move $a0, $t0
+	j getnewinput		#if play is out of bounds, ask for new input
+	
+	colfull:
+		move $t0, $a0
+		la $a0, full
+		li $v0, 4
+		syscall
+		move $a0, $t0
+	
+	getnewinput:			#if play is in a full column, ask for new input
+		beq $a0, 1, human
+		beq $a0, 2, computer
+
+#Print subroutine prints the gamespace out
+Print:
+	addiu $sp, $sp, -8		#save stack
+	sw $v0, 4($sp)
+	sw $a0, ($sp)
+
+	la $a0, newline
+	li $v0, 4
+	syscall
+	
+	li $t0, 17 #loop counter starting from end of gamespaceArray, also val of column 3
+	loop:
+	sub $t2, $t0, 2			#store column 1 in t2
+	sub $t1, $t0, 1			#store column 2 in t1
+	lb $a0, gamespace($t2)		#print col 1
+	li $v0, 1
+	syscall
+	la $a0, separate			#print separate
+	li $v0, 4
+	syscall
+	lb $a0, gamespace($t1)		#print col 2
+	li $v0, 1
+	syscall
+	la $a0, separate			#print separate
+	li $v0, 4
+	syscall
+	lb $a0, gamespace($t0)		#print col 3
+	li $v0, 1
+	syscall
+	la $a0, newline			#print new line
+	li $v0, 4
+	syscall
+	sub $t0, $t0, 3			#iterate to next row down
+	blt, $t0, 2, endLoop		#if past last row, end loop
+	j loop
+	
+	endLoop:
+	lw $a0, ($sp)			#restore stack then jump back
+	lw $v0, 4($sp)
+	addiu $sp, $sp, 8
+	jr $ra
+	
+#WinCheck subroutine checks for wins on the horizontal, vertical, and RL and LR diagonals. Takes input in v0
+WinCheck:
+	addiu $sp, $sp, -4		#save stack
+	sw $ra, ($sp)
+	
+	li $t8, 3			#save constant row size
+	
+	#horizontal
+	li $t9, 1 			#num in a line counter, 3 = win
+	move $t2, $v0
+	move $t4, $v0
+	
+	checkLeft:
+	la $t0, gamespace($t2)
+	#if at far left, then start checking right
+	div $t2, $t8
+	mfhi $t3
+	beqz $t3, checkRight
+	#else move left
+	lb $t1, -1($t0)
+	bne $t1, $a0, checkRight
+	addiu $t9, $t9, 1
+	addiu $t2, $t2, -1
+	bgt $t9, 2, PlayerWon
+	j checkLeft
+	
+	checkRight:
+	la $t0, gamespace($t4)
+	#if at far right, then end
+	div $t4, $t8
+	mfhi $t3
+	beq $t3, 2, horzEnd
+	#else check right
+	lb $t1, 1($t0)
+	bne $t1, $a0, horzEnd
+	addiu $t9, $t9, 1
+	addiu $t4, $t4, 1
+	bgt $t9, 2, PlayerWon
+	j checkRight
+	
+	horzEnd:
+	
+	#vertical
+	li $t9, 1
+	move $t2, $v0
+	move $t4, $v0
+	
+	checkTop:
+	la $t0, gamespace($t2)
+	#if at top, then starting checking below
+	bgtu $t2, 14, checkBottom
+	#else move above
+	lb $t1, 7($t0)
+	bne $t1, $a0, checkBottom
+	addiu $t9, $t9, 1
+	addiu $t2, $t2, 3
+	bgt $t9, 2, PlayerWon
+	j checkTop
+	
+	checkBottom:
+	la $t0, gamespace($t4)
+	#if at bottom, then end
+	bltu $t4, 3, vertEnd
+	#else check below
+	lb $t1, -3($t0)
+	bne $t1, $a0, vertEnd
+	addiu $t9, $t9, 1
+	addiu $t4, $t4, -3
+	bgt $t9, 2, PlayerWon
+	j checkBottom
+	
+	vertEnd:
+	
+	#diagonal from right to left
+	li $t9, 1
+	move $t2, $v0
+	move $t4, $v0
+	
+	checkTopR:
+	la $t0, gamespace($t2)
+	#if at top right corner, then start checking bottom right
+	bgtu $t2, 14, checkBotL
+	div $t2, $t8
+	mfhi $t3
+	beq $t3, 2, checkBotL
+	#else move up one and right one
+	lb $t1, 4($t0)
+	bne $t1, $a0, checkBotL
+	addiu $t9, $t9, 1
+	addiu $t2, $t2, 4
+	bgt $t9, 2, PlayerWon
+	j checkTopR
+	
+	checkBotL:
+	la $t0, gamespace($t4)
+	#if at bottom left corner, then end
+	bltu $t4, 3, diagRLEnd
+	div $t4, $t8
+	mfhi $t3
+	beq $t3, 0, diagRLEnd
+	#else check down one and left one
+	lb $t1, -4($t0)
+	bne $t1, $a0, diagRLEnd
+	addiu $t9, $t9, 1
+	addiu $t4, $t4, -4
+	bgt $t9, 2, PlayerWon
+	j checkBotL
+	
+	diagRLEnd:
+	
+	#diagonal from left to right
+	li $t9, 1
+	move $t2, $v0
+	move $t4, $v0
+	
+	checkTopL:
+	la $t0, gamespace($t2)
+	#if at top left corner, then start checking bottom right
+	bgtu $t2, 14, checkBotR
+	div $t2, $t8
+	mfhi $t3
+	beq $t3, 0, checkBotR
+	#else look above one and left one
+	lb $t1, 2($t0)
+	bne $t1, $a0, checkBotR
+	addiu $t9, $t9, 1
+	addiu $t2, $t2, 2
+	bgt $t9, 2, PlayerWon
+	j checkTopL
+	
+	checkBotR:
+	la $t0, gamespace($t4)
+	#if at bottom right corner, then end
+	bltu $t4, 3, diagLREnd
+	div $t4, $t8
+	mfhi $t3
+	beq $t3, 2, diagLREnd
+	#else look below one and right one
+	lb $t1, -2($t0)
+	bne $t1, $a0, diagLREnd
+	addiu $t9, $t9, 1
+	addiu $t4, $t4, -2
+	bgt $t9, 2, PlayerWon
+	j checkBotR
+	
+	diagLREnd:
+	
+	#tie
+	li $t9, 15
+	la $t0, gamespace($t9)
+	li $t2, 0
+	
+	checkFilled:
+	lb $t1, ($t0)
+	beqz $t1, tieEnd
+	addi $t0, $t0, 1
+	add $t2, $t2, 1
+	beq $t2, 3, GameTie
+	j checkFilled
+	
+	tieEnd:
+	
+	lw $ra, ($sp)			#restore stack then return to Round
+	addiu $sp, $sp, 4
+	jr $ra
+	
+#Three end game possibilities, a tie, player 1 wins, or player 2 wins
+	
+GameTie:
+	la $a0, tietext
+	li $v0, 4
+	syscall
+	j End
+	
+PlayerWon:
+	beq $a0, 1, player1Win
+	
+	#player 2 won
+	la $a0, cpuwintext
+	li $v0, 4
+	syscall
+	j End
+	
+	player1Win:
+	la $a0, humanwintext
+	li $v0, 4
+	syscall
+	
+End:
+	li $v0, 10
+	syscall
